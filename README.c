a09531af386c404232989df463c0856fcf04d727
LALITHASHREE <dlalithashree@gmail.com>
2016-07-08 11:15:48 +0530

    finish

---
+++ README.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+clrscr();
+printf("enter the given charcter);
+switch(c)
+{
+case 1:
+ {
+ printf("enter the given number is an alphabet");
+ break;
+ }
+case 2:
+{
+printf("enter the given number is not an alphabet");
+break;
+}
+default:
+{
+printf("the given character is not  a string");
+}return0;
+}
+
+   
